a184b5fa20823c458878cd1092dd773a10ce7b60
Xavier Damman <xdamman@gmail.com>
2014-01-03 18:29:50 -0800

    Cleaning a bit the code for consistency

--- src/selection-sharer.js
+++ src/selection-sharer.js
@@ -23,34 +23,8 @@
     this.textSelection='';
     this.htmlSelection='';
 
-    var getRelatedTwitterAccounts = function() {
-      var usernames = [];
 
-      var creator = $('meta[name="twitter:creator"]').attr("content") || $('meta[name="twitter:creator"]').attr("value");
-      if(creator) usernames.push(creator);
-
-
-      // We scrape the page to find a link to http://twitter.com/username
-      var anchors = document.getElementsByTagName('a');
-      for(var i=0, len=anchors.length;i<len;i++) { 
-        if(anchors[i].attributes.href && typeof anchors[i].attributes.href.value == 'string') {
-          var matches = anchors[i].attributes.href.value.match(/^https?:\/\/twitter\.com\/([a-z0-9_]{1,20})/i) 
-          if(matches && matches.length > 1 && ['widgets','intent'].indexOf(matches[1])==-1)
-            usernames.push(matches[1]); 
-        } 
-      }
-
-      if(usernames.length > 0)
-        return usernames.join(',');
-      else
-        return '';
-    }
-
-    this.viaTwitterAccount = $('meta[name="twitter:site"]').attr("content") || $('meta[name="twitter:site"]').attr("value") || "";
-    this.viaTwitterAccount = this.viaTwitterAccount.replace(/@/,'');
-    this.relatedTwitterAccounts = getRelatedTwitterAccounts();
-
-    var getSelectionText = function() {
+    this.getSelectionText = function() {
         var html = "", text = "";
         if (typeof window.getSelection != "undefined") {
             var sel = window.getSelection();
@@ -70,9 +44,9 @@
         self.textSelection = text;
         self.htmlSelection = html || text;
         return text;
-    }
+    };
 
-    var selectionDirection = function(selection) {
+    this.selectionDirection = function(selection) {
       var sel = selection || window.getSelection();
       var range = document.createRange();
       if(!sel.anchorNode) return 0;
@@ -81,12 +55,12 @@
       var direction = (range.collapsed) ? "backward" : "forward";
       range.detach();
       return direction;
-    }
+    };
 
     this.show = function(e) {
       setTimeout(function() {
         var sel = window.getSelection(); 
-        var selection = getSelectionText();
+        var selection = self.getSelectionText();
         if(!sel.isCollapsed && selection.length>10) {
           var range = sel.getRangeAt(0);
           var topOffset = range.getBoundingClientRect().top - 5;
@@ -103,7 +77,7 @@
             }
             while(obj = obj.offsetParent);
           }
-          switch(selectionDirection(sel)) {
+          switch(self.selectionDirection(sel)) {
             case 'forward':
               left -= self.$popover.width();
               break;
@@ -119,23 +93,56 @@
           }, 0);
         }
       }, 10);
-    }
+    };
 
     this.hide = function(e) {
       self.$popover.hide();
     };
 
-    var smart_truncate = function(str, n){
+    this.smart_truncate = function(str, n){
         if (!str || !str.length) return str;
         var toLong = str.length>n,
             s_ = toLong ? str.substr(0,n-1) : str;
         s_ = toLong ? s_.substr(0,s_.lastIndexOf(' ')) : s_;
         return  toLong ? s_ +'...' : s_;
-     }
+    };
 
-    var shareTwitter = function(e) {
+    this.getRelatedTwitterAccounts = function() {
+      var usernames = [];
+
+      var creator = $('meta[name="twitter:creator"]').attr("content") || $('meta[name="twitter:creator"]').attr("value");
+      if(creator) usernames.push(creator);
+
+
+      // We scrape the page to find a link to http(s)://twitter.com/username
+      var anchors = document.getElementsByTagName('a');
+      for(var i=0, len=anchors.length;i<len;i++) { 
+        if(anchors[i].attributes.href && typeof anchors[i].attributes.href.value == 'string') {
+          var matches = anchors[i].attributes.href.value.match(/^https?:\/\/twitter\.com\/([a-z0-9_]{1,20})/i) 
+          if(matches && matches.length > 1 && ['widgets','intent'].indexOf(matches[1])==-1)
+            usernames.push(matches[1]); 
+        } 
+      }
+
+      if(usernames.length > 0)
+        return usernames.join(',');
+      else
+        return '';
+    };
+
+    this.shareTwitter = function(e) {
       e.preventDefault(); 
-      var text = "“"+smart_truncate(self.textSelection.trim(), 114)+"”";
+
+      if(!self.viaTwitterAccount) {
+        self.viaTwitterAccount = $('meta[name="twitter:site"]').attr("content") || $('meta[name="twitter:site"]').attr("value") || "";
+        self.viaTwitterAccount = self.viaTwitterAccount.replace(/@/,'');
+      }
+
+      if(!self.relatedTwitterAccounts) {
+        self.relatedTwitterAccounts = self.getRelatedTwitterAccounts();
+      }
+
+      var text = "“"+self.smart_truncate(self.textSelection.trim(), 114)+"”";
       var url = 'http://twitter.com/intent/tweet?text='+encodeURIComponent(text)+'&related='+self.relatedTwitterAccounts+'&url='+encodeURIComponent(window.location.href);
 
       // We only show the via @twitter:site if we have enough room
@@ -150,7 +157,7 @@
       return false;
     };
 
-    var shareEmail = function(e) {
+    this.shareEmail = function(e) {
       var text = self.htmlSelection.replace(/<p[^>]*>/ig,'\n').replace(/<\/p>|  /ig,'').trim();
       var email = {};
       email.subject = encodeURIComponent("Quote from "+document.title);
@@ -172,8 +179,8 @@
                        + '</div>';
 
       self.$popover = $(popoverHTML);
-      self.$popover.find('a.tweet').click(shareTwitter);
-      self.$popover.find('a.email').click(shareEmail);
+      self.$popover.find('a.tweet').click(self.shareTwitter);
+      self.$popover.find('a.email').click(self.shareEmail);
 
       $('body').append(self.$popover); 
     };

2a928cae6cd2629fa3276e5587eaee141ea186e4
Xavier Damman <xdamman@gmail.com>
2015-01-29 15:16:04 -0800

    fix blank space

--- src/selection-sharer.js
+++ src/selection-sharer.js
@@ -229,7 +229,7 @@
       var w = 640, h=440;
       var left = (screen.width/2)-(w/2);
       var top = (screen.height/2)-(h/2)-100;
-      window.open(url, "share_twitter", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+',       top='+top+', left='+left);
+      window.open(url, "share_twitter", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
       self.hide();
       return false;
     };

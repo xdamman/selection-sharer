b18b3c822f2c19f946b5214c3a36f1551edfc5a4
shri3k <sickgrin@gmail.com>
2019-11-30 11:28:55 -0600

    [chore] Clean up few scripts and formatting tools

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,3 +1,10 @@
-#! /usr/bin/env sh
-npm run prettier
+#!/usr/bin/env bash
 
+JSFILES=($(git diff --name-only --cached | grep '.*src/.*js$'))
+if [ "${#JSFILES[@]}" -gt 0 ]; then
+  echo "Formatting through prettier.."
+  echo "${JSFILES[@]}"
+  npm run prettier "${JSFILES[@]}"
+else
+  echo "Skipping formatting.."
+fi
